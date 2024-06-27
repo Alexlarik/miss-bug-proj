bcb86d9de43147f46f1a9508d60333ac7d26ff97
alexl <alexlarik13@gmail.com>
2024-06-27 18:45:30 +0300

    Add public frontend inner project for later use and test inital server bugs

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 import express from 'express'
+
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
+
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+
+var testBugs = [
+    { _id: 'bug101', title: 'bug test', description: 'nice bug', severity: 1, createdAt: Date.now() }
+]
+
+
+// Express Routing:
+
+app.get('/api/bug', (req, res) => {
+    res.send(testBugs)
+})
+
+app.get('/api/bug/:id', (req, res) => {
+    const { id } = req.params
+    const bug = testBugs.find(bug => bug._id === id)
+    res.send(bug)
+})
+
+const port = 3030
+app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:${port}/`))
